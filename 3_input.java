10b5ed7a546681d015fabdc92f5d1df1e8c0cff9
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2022-11-07 17:10:24 +0530

    Create 3_input.java

---
+++ 3_input.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+public class q1_GradingSys {
+    public static void main(String[] args) {
+
+        System.out.println("Enter any random number : ") ;
+        Scanner scn = new Scanner(System.in) ;
+        int n1= scn.nextInt() ;
+        System.out.print("Your number is " + n1) ;
+    }
+    
+}
